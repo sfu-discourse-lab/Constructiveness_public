5b915cf0f97c4d5c0d8d23c3a55723789c35f10b
Varada Kolhatkar <kvarada@gmail.com>
2018-08-11 18:23:12 -0700

    adding config file and making path changes

--- source/experiments/dataset_combinations_experiments.py
+++ source/experiments/dataset_combinations_experiments.py
@@ -3,43 +3,26 @@ import pandas as pd
 import sys
 sys.path.append('../modeling/')
 from svm_constructiveness_classification import ConstructivenessClassifier
+sys.path.append('../../')
+from config import Config
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Classify constructive comments')
     parser.add_argument('--train_dataset_path', '-tr', type=str, dest='train_dataset_path', action='store',
-                        #default= '/Users/vkolhatk/Data/Constructiveness/data/train/features.csv',
-                        #default='/Users/vkolhatk/Data/Constructiveness/data/train/gnm_features.csv',
-                        #default = '/Users/vkolhatk/Data/Constructiveness/data/train/gnm_nyt_ync_features.csv',
-                        default='/Users/vkolhatk/Data/Constructiveness/data/train/SOCC_nyt_ync_features.csv',
+                        default= Config.TRAIN_PATH + 'SOCC_nyt_ync_features.csv',
                         help="The input CSV containing features")
 
     parser.add_argument('--model_path', '-m', type=str, dest='model_file_path', action='store',
-                        default= '/Users/vkolhatk/Data/Constructiveness/intermediate_output/models/svm_model.pkl',
-                        #default='/home/vkolhatk/data/Constructiveness/data/train/features.csv',
+                        default= Config.MODEL_PATH + 'svm_model.pkl',
                         help="the path of the file to save the model")
 
     parser.add_argument('--test_dataset_path', '-te', type=str, dest='test_dataset_path', action='store',
-                        default='/Users/vkolhatk/Data/Constructiveness/data/test/features.csv',
-                        #default='/home/vkolhatk/data/Constructiveness/data/test/features.csv',
+                        default= Config.TEST_PATH + 'features.csv',
                         help="The test dataset path for constructive and non-constructive comments")
-
-    parser.add_argument('--features', '-f', type=list, dest='features', action='store',
-                        default=(['Has_conjunction_or_connectives',
-                                  'Has_stance_adverbials',
-                                  'Has_reasoning_verbs', 'Has_modals'
-                                  'Has_shell_nouns',
-                                  'Len',
-                                  'Average_word_length',
-                                  'Redability',
-                                  'PersonalEXP',
-                                  'Named_entity_count',
-                                  'nSents',
-                                  'Avg_words_per_sent']),
-
-                        help="The features to use for classification")
 
     args = parser.parse_args()
     return args
+
 
 def run_cross_validation_experiments(training_feats_df, data_sources = ['SOCC'], n = 10):
 
