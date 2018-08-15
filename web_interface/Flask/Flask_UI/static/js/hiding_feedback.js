0274ff7dab567ac3f932b76066d0061fb433eb2d
Varada Kolhatkar <kvarada@gmail.com>
2018-08-14 18:52:56 -0700

    restructuring the web interface code

--- web_interface/Flask/Flask_UI/static/js/hiding_feedback.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#want_feedback").click(function(){
-        $("#feedback_box").show();
-    });
-  });
-
--- web_interface/Flask/Flask_UI/static/js/showing_feedback_button.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#submit_button").click(function(){
-        $("#want_feedback").show();
-    });
-  });
-
